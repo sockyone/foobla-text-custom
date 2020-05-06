b99e9ca9276f61ab8a876b9cbb2e886928c6ae3f
NamPhan <sockyone16899@gmail.com>
2020-05-06 10:25:36 +0700

    download butotn and printer canvas

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     this.canvasRef = React.createRef();
     this.canvasRef2 = React.createRef();
     this.canvasRef3 = React.createRef();
+    this.printer = React.createRef();
 
     this.hiddenImage = React.createRef();
   }
@@ -53,10 +54,23 @@ class App extends React.Component {
     ctx.textAlign = 'center';
     ctx.fillText(this.value1, 730, 280);
     ctx.font = `bold 362px Wreath_Haftone`;
-    ctx.fillText(this.value2, 750, 350);
+    ctx.fillText(this.value2, 750, 400);
     let ctx2 = this.canvasRef2.current.getContext("2d");
     ctx2.clearRect(0,0,600,600);
     ctx2.drawImage(this.canvasRef3.current, 0, 0, 600, 600);
+  }
+
+  save() {
+    let printer = this.printer.current;
+    let ctx = printer.getContext("2d");
+    ctx.drawImage(this.hiddenImage.current, 0, 0, 1500, 1500);
+    ctx.drawImage(this.canvasRef3.current, 0, 0, 1500, 1500);
+    let newImage = printer.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    let link = document.createElement('a');
+    link.href = newImage;
+    link.download = "mynew.png";
+    link.click();
+    link.remove();
   }
 
   makeTextChange(value) {
@@ -74,10 +88,12 @@ class App extends React.Component {
       <div className="App" style={{ display: "relative" }}>
         <input onChange={(e)=>{this.makeTextChange(e.target.value)}}></input>
         <input onChange={(e)=>{this.makeTextChange2(e.target.value)}}></input>
+        <button onClick={()=>this.save()}>Download</button>
         <div style={{position: "relative"}}>
           <canvas style={{position: "absolute", top: "0px", left:"0px", width: "300px", height:"300px"}} ref={this.canvasRef} width={600} height={600} />
           <canvas style={{position: "absolute", top: "0px", left:"0px", width: "300px", height:"300px"}} ref={this.canvasRef2} width={600} height={600} />
           <canvas style={{position: "absolute", top: "0px", left:"0px", display: "none"}} ref={this.canvasRef3} width={1500} height={1500} />
+          <canvas style={{position: "absolute", top: "0px", left:"0px", display: "none"}} ref={this.printer} width={1500} height={1500} />
         </div>
 
         <img ref={this.hiddenImage} src={baseImage} style={{ display: "none" }} />
